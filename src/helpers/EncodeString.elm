de5e6a32af6d91df188c2fbf40376f9194b2b585
Saad Bakht <rotk13@gmail.com>
2018-11-24 12:17:12 -0500

    more encoding

--- src/helpers/EncodeString.elm
+++ src/helpers/EncodeString.elm
@@ -1,3 +1,4 @@
 module EncodeString exposing (encode)
 
-encode = ["\u{064A}\u{064E}\u{0627}", "\u{06D6}", "\u{06DA}"]
+encode = ["\u{064A}\u{064E}\u{0627}", "\u{06D6}", "\u{06DA}", "\u{06D8}", "\u{06DC}", "\u{06D7}", "۞"]
+--encode = []
